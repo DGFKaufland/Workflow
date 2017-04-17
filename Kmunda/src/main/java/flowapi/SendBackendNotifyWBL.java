0f42e4c8486a327002cd0e8def11ef8dfcbd49aa
Jonas <jonas_fritzsch@gmx.de>
2017-04-17 11:57:54 +0200

    Mobile Message Text modified

--- Kmunda/src/main/java/flowapi/SendBackendNotifyWBL.java
+++ Kmunda/src/main/java/flowapi/SendBackendNotifyWBL.java
@@ -9,14 +9,14 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 public class SendBackendNotifyWBL extends SendBackendNotify {
 
 	String messageText;
-	public static String sMobileMsg = "WARNUNG, Temperatur zu hoch! Werte(r) ";
+	public static String sMobileMsg = "WARNUNG, Temperatur zu hoch! ";
 
 	public void execute(DelegateExecution execution) throws Exception {
 		data = (WorkflowRecord) execution.getVariable("flowData");
 
 		String timeStamp = new SimpleDateFormat("EEE, MMM d, HH:mm").format(new Date());
-		messageText = "[" + timeStamp + "] " + sMobileMsg + data.getHL_MobileName() + " aus Abteilung "
-				+ data.getAbteilungsName() + ", " + data.getTemperaturwert() + " Grad.";
+		messageText = "[" + timeStamp + "] " + sMobileMsg + " Abteilung "
+				+ data.getAbteilungsName() + ": " + data.getTemperaturwert() + " Grad.";
 		System.out.println("Flow SendBackendNotify: \"" + messageText + "\"");
 		sendMessage(data.getWBL_Mobile_ID(), messageText);
 		execution.setVariable("Notification_ID", notification_ID);
