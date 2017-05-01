093a0382185042a232663fe4668b7851010ef4f9
Jonas Fritzsch <jonas_fritzsch@gmx.de>
2017-05-01 12:39:16 +0200

    Time for Notifications set to CET

--- Kmunda/src/main/java/flowapi/SendBackendNotifyWBL.java
+++ Kmunda/src/main/java/flowapi/SendBackendNotifyWBL.java
@@ -4,19 +4,26 @@ import kmunda.WorkflowRecord;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
+
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 
 public class SendBackendNotifyWBL extends SendBackendNotify {
 
 	String messageText;
-	public static String sMobileMsg = "WARNUNG, Temperatur zu hoch! ";
+	public static String sMobileMsg = "WARNUNG, Temperatur zu hoch in Abteilung ";
 
 	public void execute(DelegateExecution execution) throws Exception {
 		data = (WorkflowRecord) execution.getVariable("flowData");
 
-		String timeStamp = new SimpleDateFormat("EEE, MMM d, HH:mm").format(new Date());
-		messageText = "[" + timeStamp + "] " + sMobileMsg + " Abteilung "
-				+ data.getAbteilungsName() + ": " + data.getTemperaturwert() + " Grad.";
+	    SimpleDateFormat sdf = new SimpleDateFormat("EEE, MMM d, HH:mm");   
+	    TimeZone tz = TimeZone.getTimeZone("Europe/Berlin");
+	    sdf.setTimeZone(tz);
+	    sdf.format(new Date()); // will return a string rep of a date with the included format, better than:
+	                            // String timeStamp = new SimpleDateFormat("EEE, MMM d, HH:mm").format();
+	    
+		messageText = "[" + sdf.toString() + "] " + sMobileMsg +
+				      data.getAbteilungsName() + ": " + data.getTemperaturwert() + " Grad.";
 		System.out.println("Flow SendBackendNotify: \"" + messageText + "\"");
 		sendMessage(data.getWBL_Mobile_ID(), messageText);
 		execution.setVariable("Notification_ID", notification_ID);
